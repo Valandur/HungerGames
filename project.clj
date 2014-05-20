18f81f61f2a44bc9c055280c0fabbc704c5ff66c
Valandur <valandur@snaxe.ch>
2014-05-20 15:21:14 +0200

    Using leiningen, updated travis

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject valandur/hunger_games "0.2.4"
+  :description "Hunger games"
+  :url "https://github.com/Valandur/HungerGames"
+  :license {:name "Private to Andy, Marco, Tim"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-vanity "0.2.0"]]
+  :javac-target "1.7"
+  :java-source-paths ["src" "test"]
+  :target-path "target/%s")
